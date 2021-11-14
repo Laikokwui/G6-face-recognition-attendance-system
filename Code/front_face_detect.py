3fb8aea99ec0deb7e08ff015df610965f741664c
Lai Kok Wui <kokwei325@gmail.com>
2021-11-14 13:19:19 +0800

    added opencv default front face detection

--- Code/front_face_detect.py
+++ Code/front_face_detect.py
@@ -25,13 +25,13 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minSize=(30, 30), minNeighbors=7)
     
     # Draw the rectangle around each face
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         
-    text = "Press \"Q\" to stop"
+    text = "Press \"Q\" to stop" + "Number of face " + str(len(faces))
     cv2.putText(img, text, (10, img.shape[0] - 25),
 		cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 2)
     
