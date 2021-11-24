43703fc60b13d22853f71425a3eb2303b0c1c9ae
Lai Kok Wui <47911209+Laikokwui@users.noreply.github.com>
2021-11-24 21:48:06 +0800

    added useful comment

--- Pages/addpeople_camera.py
+++ Pages/addpeople_camera.py
@@ -10,30 +10,44 @@ import imutils
 import os
 from Code import utilities
 
+# change the root path
+ROOT = "C:/Users/Asus/Documents/G6-face-recognition-attendance-system/"
+
 def app():
+    # title of the page
     st.title('Add People by Using Webcam')
     
+    # form structure
     with st.form(key='add_people'):
         name = st.text_input('Name')
         submit_button = st.form_submit_button('Upload New Person')
     
+    # run this when the submit button is clicked
     if submit_button:
+        # check the name is not empty
         if name == "":
             st.error("Name cannot be empty!")
         else:
-            labelsPath = "C:/Users/Asus/Documents/G6-face-recognition-attendance-system/yolov4/obj.names"
-            weightsPath = "C:/Users/Asus/Documents/G6-face-recognition-attendance-system/yolov4/yolov4-obj_last.weights"
-            configPath = "C:/Users/Asus/Documents/G6-face-recognition-attendance-system/yolov4/yolov4-obj.cfg"
+            # setup yolov4
+            labelsPath = ROOT + "yolov4/obj.names"
+            weightsPath = ROOT + "yolov4/yolov4-obj_last.weights"
+            configPath = ROOT + "yolov4/yolov4-obj.cfg"
+            
+            # call the setup function from utilities module
             net, ln, labels = utilities.Yolov4Setup(labelsPath, weightsPath, configPath, True)
             
             capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-           
+            # if receive any signal from webcam start the loop
             while capture.isOpened():
-                stop = False
-                grabbed, frame = capture.read()
+                stop = False # stop the webcam is false
+                grabbed, frame = capture.read() # get the frame
+                # if there no signal
                 if not grabbed:
-                    break
+                    break # stop the loop
+                # resize the capture image
                 frame = imutils.resize(frame,width=700)
+                
+                # face detection
                 results = utilities.detect_face(frame, net, ln, 0.3, 0.3, objIdx=labels.index("face"))
                 
                 if len(results) == 1:
@@ -46,18 +60,18 @@ def app():
                         img = frame
                         crop_img = img[startY:endY, startX:endX]
                         
+                        # save the cropped image if the button s is pressed
                         if cv2.waitKey(1) & 0xFF == ord('s'):
-                            path = r"C:\Users\Asus\Documents\G6-face-recognition-attendance-system\Datasets\Database"
+                            path = ROOT + "Datasets/Database"
                             imagename = name + ".jpg"
                             imagepath = os.path.join(path,imagename)
                             cv2.imwrite(imagepath, crop_img)
                             stop = True
                         
+                        # put the bounding box on the frame capture by webcam
                         cv2.putText(img, "Face_Detect %s" % ("{:.3f}".format(prob)), (startX, startY-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
-                    		
                         cv2.rectangle(img, (startX, startY), (endX, endY), color, 2)
-                    
-                    
+                    # show the webcam
                     cv2.imshow("Image Collection", img)
                     
                 if stop:
