0282af89f96ee2e20aa91b76f61d001eda8b248b
Lai Kok Wui <kokwei325@gmail.com>
2021-11-24 09:28:54 +0800

    update gui and remove opencv face detection

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ import numpy as np
 import streamlit as st
 
 from multipage import MultiPage
-from Pages import home, about
+from Pages import home, about, addpeople_image_upload, addpeople_camera
 
+st.set_page_config(layout="wide")
 app = MultiPage()
 
 # Title of the main page
-st.title("Group 6 - ML Assignment")
+st.title("Face Recognition Attendance System")
 
 # Add all your application here
 app.add_page("Home", home.app)
+app.add_page("Add People (Image Upload)", addpeople_image_upload.app)
+app.add_page("Add People (Camera Input)", addpeople_camera.app)
 app.add_page("About Us", about.app)
 
 # The main app
